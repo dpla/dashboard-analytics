2089d1155aad96333ee01e567751b3e7bf9ce108
Audrey Altman <audrey@dp.la>
2018-04-03 09:54:51 -0400

    refactor

--- lib/ga_response_builder.rb
+++ lib/ga_response_builder.rb
@@ -16,6 +16,8 @@ class GaResponseBuilder
     @end_date = "yesterday"
   end
 
+  ##
+  # @return [String]
   def token
     # By default, the access token will expire after 1 hour.
     if(@@authorizer.access_token.nil? or @@authorizer.expired?)
@@ -24,37 +26,58 @@ class GaResponseBuilder
     @@authorizer.access_token
   end
 
+  ##
+  # @return [Hash]
   def overall_metrics
     metrics = %w(ga:totalEvents ga:uniqueEvents ga:sessions ga:users)
     dimensions = %w()
     filters = %W(ga:eventCategory=@#{@hub} ga:eventCategory!@Browse)
 
-    result = @analytics.get_ga_data(@profile_id,
-                                    @start_date,
-                                    @end_date,
-                                    metrics.join(','),
-                                    dimensions: dimensions.join(','), #comma = "or"
-                                    filters: filters.join(';')) #semicolon = "and"
+    r = result(metrics, dimensions, filters)
 
-    result.totals_for_all_results.map do |key, value| 
+    # Create human-readable key-value pairs
+    # Example: change "ga:totalEvents" to "Total Events"
+    r.totals_for_all_results.map do |key, value| 
       [key.split(':')[1].underscore.humanize.titleize, value]
     end.to_h
   end
 
+  ##
+  # @return [Hash]
   def event_metrics
     metrics = %w(ga:totalEvents)
     dimensions = %w(ga:eventCategory)
     filters = %W(ga:eventCategory=@#{@hub} ga:eventCategory!@Browse)
 
-    result = @analytics.get_ga_data(@profile_id,
-                                    @start_date,
-                                    @end_date,
-                                    metrics.join(','),
-                                    dimensions: dimensions.join(','), #comma = "or"
-                                    filters: filters.join(';')) #semicolon = "and"
+    r = result(metrics, dimensions, filters)
 
-    result.rows.collect{ |r| 
-      [r[0].split(' : ')[0], r[1]]
+    # Create human-readable key-value pairs
+    # Example: change "Click Through : ArtStor" to "Click Through"
+    r.rows.collect{ |row| 
+      [row[0].split(' : ')[0], row[1]]
     }.to_h
+  end
+
+  ##
+  # Get result for a google analytics query.
+  #
+  # @param metrics [Array<String>]
+  # @param dimensions [Array<String>]
+  # @param filters [Array<String>]
+  #
+  # @return [Google::Apis::AnalyticsV3::GaData]
+  #
+  # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+  # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+  # @raise [Google::Apis::AuthorizationError] Authorization is required
+  #
+  # TODO: handle failed request
+  def result(metrics, dimensions, filters)
+    @analytics.get_ga_data(@profile_id,
+                           @start_date,
+                           @end_date,
+                           metrics.join(','), #comma = "or"
+                           dimensions: dimensions.join(','), #comma = "or"
+                           filters: filters.join(';')) #semicolon = "and"
   end
 end
