3dacfaab343062e9f0e224e49b23bfbfc8afcb5e
Audrey Altman <audrey@dp.la>
2018-04-05 20:25:59 -0400

    edit user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,6 +13,10 @@ module Admin
       @user = User.new
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
     def create
       @user = User.new(user_params)
 
@@ -20,6 +24,16 @@ module Admin
         redirect_to admin_users_path
       else
         render 'new'
+      end
+    end
+
+    def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        redirect_to admin_users_path
+      else
+        render 'edit'
       end
     end
 
