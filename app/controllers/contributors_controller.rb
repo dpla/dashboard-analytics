7dd8bc9ec2e9d32cf4d75f769e3f3dd52e072797
Audrey Altman <audrey@dp.la>
2018-04-07 09:49:25 -0400

    config travis

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -6,7 +6,6 @@ class ContributorsController < ApplicationController
     @start_date = "30daysAgo"
     @end_date = "yesterday"
     @hub = Hub.new(params[:hub_id], @start_date, @end_date)
-    @contributors = @hub.contributors
 
     unless current_user.hub == params[:hub_id] || current_user.hub == "All"
       redirect_to hub_contributors_path(current_user.hub)
