b2763dad13e3f37d9ddefa1a3e262015b7fb014a
Audrey Altman <audrey@dp.la>
2018-04-06 21:04:37 -0400

    contributor index

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -2,6 +2,17 @@
 
 class ContributorsController < ApplicationController
 
+  def index
+    @start_date = "30daysAgo"
+    @end_date = "yesterday"
+    @hub = Hub.new(params[:hub_id], @start_date, @end_date)
+    @contributors = @hub.contributors
+
+    unless current_user.hub == params[:hub_id] || current_user.hub == "All"
+      redirect_to hub_path(current_user.hub)
+    end
+  end
+
   def show
     @start_date = "30daysAgo"
     @end_date = "yesterday"
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :hubs, only: [:index, :show] do
-    resources :contributors, only: :show  
+    resources :contributors, only: [:index, :show]  
   end
 
   root 'hubs#index'
