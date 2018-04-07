1c68aeb149581c88e8af9531d74dcfd58627f4b7
Audrey Altman <audrey@dp.la>
2018-04-06 21:18:55 -0400

    add tests

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -9,7 +9,7 @@ class ContributorsController < ApplicationController
     @contributors = @hub.contributors
 
     unless current_user.hub == params[:hub_id] || current_user.hub == "All"
-      redirect_to hub_path(current_user.hub)
+      redirect_to hub_contributors_path(current_user.hub)
     end
   end
 
--- spec/routing/contributors_routing_spec.rb
+++ spec/routing/contributors_routing_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe "contributors routing", type: :routing do
 
+  it "routes /hubs/foo/contributors to contributors#index" do
+    expect(get: "hubs/foo/contributors/").to route_to(
+      controller: "contributors",
+      action: "index",
+      hub_id: "foo"
+    )
+  end
+
   it "routes /hubs/foo/contributors/bar to contributors#show for bar" do
     expect(get: "hubs/foo/contributors/bar").to route_to(
       controller: "contributors",
