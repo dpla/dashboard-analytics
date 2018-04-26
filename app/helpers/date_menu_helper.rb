96c742b766a00a8cf85b6e0fb39a4e5d552ed8a1
Audrey Altman <audrey@dp.la>
2018-04-26 16:14:06 -0400

    fix return statement

--- app/helpers/date_menu_helper.rb
+++ app/helpers/date_menu_helper.rb
@@ -2,12 +2,13 @@ module DateMenuHelper
   def dates
     first_date = Date.new(Settings.min_date.month, Settings.min_date.year)
     last_date = Date.today.beginning_of_month
-
     dates = [first_date]
 
     loop do
       dates.push(dates.last.next_month.beginning_of_month)
       break if dates.last == last_date 
     end
+
+    dates
   end
 end
