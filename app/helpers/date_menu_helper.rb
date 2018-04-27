29d15141e0d311714cb1063392ec46291fcb5b92
Audrey Altman <audrey@dp.la>
2018-04-27 16:39:35 -0400

    progress on dropdown menu

--- app/helpers/date_menu_helper.rb
+++ app/helpers/date_menu_helper.rb
@@ -1,6 +1,14 @@
 module DateMenuHelper
-  def dates
-    first_date = Date.new(Settings.min_date.month, Settings.min_date.year)
+  def date_form_options
+    available_dates.map do |date|
+      label = "#{Date::ABBR_MONTHNAMES[date.month]} #{date.year}"
+      value = date.month
+      [label, value]
+    end
+  end
+
+  def available_dates
+    first_date = Date.new(Settings.min_date.year, Settings.min_date.month)
     last_date = Date.today.beginning_of_month
     dates = [first_date]
 
@@ -10,5 +18,10 @@ module DateMenuHelper
     end
 
     dates
+  end
+
+  def selected_date
+    return unless @start_date
+    @start_date.month
   end
 end
