286d187bfd3acd5ff1c13237caeb8ad2083501e0
Audrey Altman <audrey@dp.la>
2018-05-09 17:28:08 -0400

    new files

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,22 @@
+# Handles HTTP requests for events
+
+class LocationsController < ApplicationController
+  include DateSetter # Controller concern
+  include DateHelper # View helper
+
+  def index
+    assign_start_and_end_dates
+    @hub = Hub.new(params[:hub_id], @start_date, @end_date)
+
+    if params[:contributor_id]
+      @contributor = Contributor.new(params[:contributor_id], params[:hub_id],
+                                     @start_date, @end_date)
+    end
+
+    @target = params[:contributor_id] ? @contributor : @hub
+
+    unless current_user.hub == params[:hub_id] || current_user.hub == "All"
+      redirect_to hub_path(current_user.hub)
+    end
+  end
+end
