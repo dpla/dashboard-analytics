44365950abe0c9bcea8e9c6ddbb213c16f28fef2
Audrey Altman <audrey@dp.la>
2018-05-16 17:17:45 -0400

    cleanup

--- lib/metadata_completeness.rb
+++ lib/metadata_completeness.rb
@@ -1,8 +1,7 @@
-require 'open-uri'
-
 class MetadataCompleteness
   include ActionView::Helpers::NumberHelper
 
+  # @param Hub || Contributor
   def initialize(target)
     @target = target
   end
@@ -11,16 +10,22 @@ class MetadataCompleteness
     @target
   end
 
+  # @return Hash
   def data
     @data ||= get_data
   end
 
+  # @param field String the metadata field e.g. "type"
+  # @return String percentage representation of completeness for given field
   def percentage(field)
     number_to_percentage(data[field].to_f * 100, precision: 0)
   end
 
   private
 
+  ##
+  # Gets completeness data for the target.
+  # @return Hash
   def get_data
     if target.is_a?(Hub)
       get_hub_data
@@ -47,6 +52,7 @@ class MetadataCompleteness
     return data.to_hash
   end
 
+  # @return Hash
   def get_contributor_data
     contributor_name = target.name rescue nil
     hub_name = @target.hub.name rescue nil
@@ -67,10 +73,12 @@ class MetadataCompleteness
     return data.to_hash
   end
 
+  # @return String
   def hub_filepath
     Rails.root.join("public", "spreadsheets", "provider-scores-20180504.csv")
   end
 
+  # @return String
   def contributor_filepath
     Rails.root.join("public", "spreadsheets", "contributor-scores-20180504.csv")
   end
