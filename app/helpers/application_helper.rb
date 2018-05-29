28b550e75d77fee6997fe048fbd004736e327e1b
Audrey Altman <audrey@dp.la>
2018-05-29 13:02:22 -0400

    add curent date app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_start_date
+    @start_date.strftime("%Y-%m") rescue nil
+  end
+
+  def current_end_date
+    @end_date.strftime("%Y-%m") rescue nil
+  end
 end
--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -27,14 +27,6 @@ module DateHelper
     dates
   end
 
-  def selected_start_date
-    @start_date.strftime("%Y-%m")
-  end
-
-  def selected_end_date
-    @end_date.strftime("%Y-%m")
-  end
-
   def api_data_for_date_range?
     api_min_date = Date.new(Settings.api_min_date.year, 
                             Settings.api_min_date.month)
