4a9ae122f81be426f67d5539bce2c5dfbc237808
Audrey Altman <audrey@dp.la>
2018-05-29 15:01:26 -0400

    preserve date across pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
   def current_end_date
     @end_date.strftime("%Y-%m") rescue nil
   end
+
+  def date_opts
+    { start_date: current_start_date, end_date: current_end_date }
+  end
 end
--- app/helpers/data_menu_helper.rb
+++ app/helpers/data_menu_helper.rb
@@ -1,58 +1,72 @@
 module DataMenuHelper
   
   def render_overview_link(target)
-    path = target.is_a?(Hub) ? hub_path(target.name) : 
-      hub_contributor_path(target.hub.name, target.name)
+    path = target.is_a?(Hub) ?
+      hub_path(target.name, date_opts) : 
+      hub_contributor_path(target.hub.name, target.name, date_opts)
+
     link_to("Overview", path, html_opts(path))
   end
 
   def render_locations_link(target)
-    path = target.is_a?(Hub) ? hub_locations_path(target.name) :
-      hub_contributor_locations_path(target.hub.name, target.name)
+    path = target.is_a?(Hub) ?
+      hub_locations_path(target.name, date_opts) :
+      hub_contributor_locations_path(target.hub.name, target.name, date_opts)
+
     link_to("Website user locations", path, html_opts(path))
   end
 
   def render_view_item_link(target)
-    path = target.is_a?(Hub) ? hub_event_path(target.name, 'view_item') :
-      hub_contributor_event_path(target.hub.name, target.name, 'view_item')
+    path = target.is_a?(Hub) ?
+      hub_event_path(target.name, 'view_item', date_opts) :
+      hub_contributor_event_path(target.hub.name, target.name, 'view_item', date_opts)
+
     link_to("Metadata record views", path, html_opts(path))
   end
 
   def render_view_exhibit_link(target)
-    path = target.is_a?(Hub) ? hub_event_path(target.name, 'view_exhibit') : 
-      hub_contributor_event_path(target.hub.name, target.name, 'view_exhibit')
+    path = target.is_a?(Hub) ?
+      hub_event_path(target.name, 'view_exhibit', date_opts) : 
+      hub_contributor_event_path(target.hub.name, target.name, 'view_exhibit', date_opts)
+
     link_to("Exhibit item views", path, html_opts(path))
   end
 
   def render_view_pss_link(target)
-    path = target.is_a?(Hub) ? hub_event_path(target.name, 'view_pss') : 
-      hub_contributor_event_path(target.hub.name, target.name, 'view_pss')
+    path = target.is_a?(Hub) ?
+      hub_event_path(target.name, 'view_pss', date_opts) : 
+      hub_contributor_event_path(target.hub.name, target.name, 'view_pss', date_opts)
+
     link_to("Primary source views", path, html_opts(path))
   end
 
   def render_click_through_link(target)
-    path = target.is_a?(Hub) ? hub_event_path(target.name, 'click_through') : 
-      hub_contributor_event_path(target.hub.name, target.name, 'click_through')
+    path = target.is_a?(Hub) ?
+      hub_event_path(target.name, 'click_through', date_opts) :
+      hub_contributor_event_path(target.hub.name, target.name, 'click_through', date_opts)
+
     link_to("Click throughs", path, html_opts(path))
   end
 
   def render_view_api_link(target)
-    path = target.is_a?(Hub) ? hub_event_path(target.name, 'view_api') : 
-      hub_contributor_event_path(target.hub.name, target.name, 'view_api')
+    path = target.is_a?(Hub) ?
+      hub_event_path(target.name, 'view_api', date_opts) : 
+      hub_contributor_event_path(target.hub.name, target.name, 'view_api', date_opts)
+
     link_to("API item views", path, html_opts(path))
   end
 
   def render_website_terms_link
-    path = search_term_path('website')
+    path = search_term_path('website', date_opts)
     link_to("Website", path, html_opts(path))
   end
 
   def render_api_terms_link
-    path = search_term_path('api')
+    path = search_term_path('api', date_opts)
     link_to("API", path, html_opts(path))
   end
 
   def html_opts(path)
-    current_page?(path) ? { class: 'selected' } : {}
+    path.split("?").first == request.path ? { class: 'selected' } : {}
   end
 end
