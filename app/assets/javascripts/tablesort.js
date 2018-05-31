848c14b5134c6fcdef4db776b2365df58294219b
Audrey Altman <audrey@dp.la>
2018-05-31 13:35:35 -0400

    cleanup

--- app/assets/javascripts/tablesort.js
+++ app/assets/javascripts/tablesort.js
@@ -1,8 +1,9 @@
 (function(){
   // This is the turbolinks equivalent to a page load.
   document.addEventListener("turbolinks:load", function(){
-    console.log("turbolinks load");
+
     $(document).ready(function() {
+      
       $("#comparison").tablesorter({
         cssHeader: 'header',
         cssAsc: 'headerSortUp',
