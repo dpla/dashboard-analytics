87ad6085f2e759036018bffd1fe3df4f3b668179
AudreyAltman <AudreyAltman@users.noreply.github.com>
2018-06-19 14:25:12 -0400

    Handle ga authorizer http errors (#53)

--- app/lib/ga_authorizer.rb
+++ app/lib/ga_authorizer.rb
@@ -1,21 +1,30 @@
 require 'googleauth'
 
+##
+# Get authorization to access Google Analytics accounts.
+# The token can be used for server-side requests, or can be passed to the client
+# to authorize client-side requests.
+#
+# No attempts are not made to re-try failed HTTP requests because the googleauth
+# gem lacks good interfaces to manage this.
+#
 class GaAuthorizer
 
   ##
-  # @return [String]
+  # @return [String|nil]
   # By default, the access token will expire after 1 hour.
   def self.token
-    begin
-      if(self.authorizer.access_token.nil? or self.authorizer.expired?)
-        self.authorizer.fetch_access_token!
-      end
-      self.authorizer.access_token
-    rescue
-      # TODO: Log error
-      # TODO: Retry if failure?
-      String.new
+    if(self.authorizer.access_token.nil? or self.authorizer.expired?)
+      self.authorizer.fetch_access_token!
     end
+    
+    self.authorizer.access_token
+
+  rescue StandardError => e
+    Rails.logger.error(e)
+    # Return nil if an error occurs when attempting to get access token, such
+    # as an HTTP error.
+    return nil
   end
 
   private
