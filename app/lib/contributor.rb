de3a5bfb97c5c29ced92e089dde71e02530a03ed
Audrey Altman <audrey@dp.la>
2018-06-21 17:50:54 -0400

    delete unused code

--- app/lib/contributor.rb
+++ app/lib/contributor.rb
@@ -31,10 +31,6 @@ class Contributor
     @end_date.iso8601
   end
 
-  def ga_token
-    @frontend_ga.token
-  end
-
   def overview_website
     OverviewWebsite.new(self)
   end
@@ -53,13 +49,5 @@ class Contributor
 
   def events(event_type)
     Events.new(self, event_type)
-  end
-
-  def frontend_ga
-    @frontend_ga ||= FrontendAnalytics.new(start_date, end_date)
-  end
-
-  def api_ga
-    @api_ga ||= ApiAnalytics.new(start_date, end_date)
   end
 end
--- app/lib/hub.rb
+++ app/lib/hub.rb
@@ -33,10 +33,6 @@ class Hub
     @end_date.iso8601
   end
 
-  def ga_token
-    frontend_ga.token
-  end
-
   ##
   # Get names of all contributors that belonging to this hub
   # @return [Array<String>]
@@ -66,14 +62,6 @@ class Hub
 
   def contributor_comparison
     ContributorComparison.new(self)
-  end
-
-  def frontend_ga
-    @frontend_ga ||= FrontendAnalytics.new(start_date, end_date)
-  end
-
-  def api_ga
-    @api_ga ||= ApiAnalytics.new(start_date, end_date)
   end
 
   protected
