e5153f9abb2ffab11ab3f24788c526806833eb26
Audrey Altman <audrey@dp.la>
2018-08-09 16:29:15 -0400

    search terms async

--- app/controllers/search_terms_controller.rb
+++ app/controllers/search_terms_controller.rb
@@ -10,25 +10,33 @@ class SearchTermsController < ApplicationController
 
   def show
     assign_start_and_end_dates
-    @search_terms = get_search_terms
+  end
+
+  def website_search_terms
+    assign_start_and_end_dates
+
+    @search_terms = WebsiteSearchTerms.build do |builder|
+      builder.start_date = @start_date
+      builder.end_date = @end_date
+    end
 
     respond_to do |format|
-      format.html
+      format.html { render partial: "shared/search_terms_table"}
       format.csv { send_data @search_terms.to_csv }
     end
   end
 
-  def get_search_terms
-    if params[:id] == "website"
-      WebsiteSearchTerms.build do |builder|
-        builder.start_date = @start_date
-        builder.end_date = @end_date
-      end
-    elsif params[:id] == "api"
-      ApiSearchTerms.build do |builder|
-        builder.start_date = @start_date
-        builder.end_date = @end_date
-      end
-    end 
+  def api_search_terms
+    assign_start_and_end_dates
+
+    @search_terms = ApiSearchTerms.build do |builder|
+      builder.start_date = @start_date
+      builder.end_date = @end_date
+    end
+
+    respond_to do |format|
+      format.html { render partial: "shared/search_terms_table"}
+      format.csv { send_data @search_terms.to_csv }
+    end
   end
 end
