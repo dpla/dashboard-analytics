d55d2473d2fd56c04a7f3acda52664f7adb2965b
AudreyAltman <AudreyAltman@users.noreply.github.com>
2020-05-29 16:39:51 -0400

    show mediaAccess completeness (#131)

--- app/lib/metadata_completeness_presenter.rb
+++ app/lib/metadata_completeness_presenter.rb
@@ -3,7 +3,8 @@ class MetadataCompletenessPresenter
   # Fields to be shown in the user interface.
   def self.fields
     [ 'title', 'type', 'subject', 'description', 'preview', 'date', 'creator',
-      'spatial', 'language', 'rights', 'standardizedRights', 'count' ]
+      'spatial', 'language', 'rights', 'standardizedRights', 'mediaAccess',
+      'count' ]
   end
 
   ##
