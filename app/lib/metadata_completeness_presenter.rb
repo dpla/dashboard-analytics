ee279ee3251b72ba362e9a4006564c7a83a5cfda
AudreyAltman <audreykaltman@gmail.com>
2020-07-20 10:54:39 -0400

    remove rights

--- app/lib/metadata_completeness_presenter.rb
+++ app/lib/metadata_completeness_presenter.rb
@@ -3,8 +3,7 @@ class MetadataCompletenessPresenter
   # Fields to be shown in the user interface.
   def self.fields
     [ 'title', 'type', 'subject', 'description', 'preview', 'date', 'creator',
-      'spatial', 'language', 'rights', 'standardizedRights', 'mediaAccess',
-      'count' ]
+      'spatial', 'language', 'standardizedRights', 'mediaAccess', 'count' ]
   end
 
   ##
