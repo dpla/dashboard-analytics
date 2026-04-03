5eb0cfbe22cb6ce3f79291fe4d90fb54d8df00fd
Dominic Byrd-McDevitt <mcdevitd@gmail.com>
2026-04-03 11:54:50 -0400

    Remove WikimediaParticipants from HubsController; hubs use data presence only

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -106,7 +106,6 @@ class HubsController < ApplicationController
     @wp_data              = mc_data[:wp]
     @wa_data              = mc_data[:wa]
     @mc_data              = mc_data[:mc]
-    @wikimedia_participant = WikimediaParticipants.hub?(hub_id)
     @target               = Hub.new(hub_id, sec_start, sec_end)
 
     render partial: "shared/hub_sections"
@@ -259,9 +258,8 @@ class HubsController < ApplicationController
     )
     @wa_data = wa_presenter.hub(params[:hub_id])
 
-    @item_count            = item_count_thread.value
-    @wikimedia_participant = WikimediaParticipants.hub?(params[:hub_id])
-    @target                = Hub.new(params[:hub_id], @start_date, @end_date)
+    @item_count = item_count_thread.value
+    @target     = Hub.new(params[:hub_id], @start_date, @end_date)
 
     render partial: "shared/wikimedia_overview"
   end
